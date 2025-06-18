7dddf3900cc1d2b1cdcc3e7ff195db340213e4f3
Killerresh <erickutrera47@gmail.com>
2025-06-17 21:26:16 -0600

    Dependencias para conexión

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -52,6 +51,8 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation (libs.androidx.material3)
     implementation(libs.mpandroidchart)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
